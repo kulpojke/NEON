467747e19dfc1f924628152fe9c6b85708ebbcfb
michael <michaelhuggins@protonmail.com>
2022-07-18 09:30:22 -0700

    added hyperpectral_mosaic_TALL.ipynb, it will be template for all sites

--- build_hyper_mosaic.py
+++ build_hyper_mosaic.py
@@ -51,8 +51,8 @@ def parse_arguments():
            |
            |__mosaic
 
-        will be created. Tee site dir may already exist, but does not have to. 
-        
+        will be created. The site dir may already exist, but does not have to.
+
         ''',
     )
 
@@ -65,7 +65,7 @@ def parse_arguments():
         is ommited it will be assumed that data_dir is the same as out_dir.
         As an example of the directory structure required, if
         --site=TEAK and --data_dir=NEON,
-        
+
         NEON
         |
         |__TEAK
@@ -73,35 +73,127 @@ def parse_arguments():
            |__hyperspectral
 
         '''
+    )
 
 
+    # parse the args
+    args = parser.parse_args()
 
-        # parse the args
-        args = parser.parse_args()
+    # make data_dir if not specified
+    if not args.data_dir:
+        args.data_dir = args.out_dir
 
-        # make data_dir if not specified
-        if not args.data_dir:
-            args.data_dir = args.out_dir
+    # make source path
+    args.src = os.path.join(args.data_dir,
+                            args.site,
+                            'hyperspectral',
+                            'DP3.30006.001')
 
-        # make source path
-        args.src = os.path.join(args.data_dir,
-                                args.site,
-                                'hyperspectral',
-                                'DP3.30006.001')
+    # adjust source path in case there is an issue-log changing path
+    if os.path.isdir(os.path.join(args.src, 'neon-aop-products')):
+        args.src = os.path.join(args.src, 'neon-aop-products')
 
-        # adjust source path in case there is an issue-log changing path
-        if os.path.isdir(os.path.join(args.src, 'neon-aop-products'):
-            args.src = os.path.join(args.src, 'neon-aop-products')
+    # make destination path
+    args.dst = os.path.join(args.out_dir,
+                            args.site,
+                            'mosaic')
 
-        # make destination path
-        args.dst = os.path.join(args.out_dir,
-                                args.site,
-                                'mosaic')
+    # make a path to hold pca output
+    args.pca = os.path.join(args.out_dir, args.site, 'pca')
 
-        # make a path to hold pca output
-        args.pca = os.path.join(args.out_dir, args.site, 'pca')
+    return args
 
-        return args
+
+def band_list():
+    '''excludes bands with H2O or CO2 absorption'''
+    good_bands = np.hstack([
+        np.arange(0, 188 + 1),
+        np.arange(211, 269 + 1),
+        np.arange(316, 425 + 1)
+    ])
+
+    return good_bands
+
+
+def sample_from_all(files, size):
+    '''
+    Returns a np array of samples of shape (N, wl) where N is
+    the number of samples and wl is the length of the wavelength
+    dimension.
+
+    args:
+        files    - list of full paths to netcdf4 files to be used.
+        size     - fraction of data to be used.
+    '''
+    # empty list for samples
+    samples = []
+
+    for fname in files:
+        samples.append(sample_from_file(fname, size))
+
+    with ProgressBar():
+        sample = np.vstack(compute(*samples))
+
+    return sample
+
+
+def plot_pca_var(pca):
+    '''plots explained variance by PCA component'''
+
+    # make fig
+    plt.figure(figsize=(10,4));
+
+    # plot
+    plt.plot(range(1, 359),
+             pca.explained_variance_ratio_.cumsum(),
+             marker='o',
+             linestyle='--');
+
+    # details
+    plt.title('Explained Variance by Number of Components');
+    plt.xlabel('Components');
+    plt.ylabel('Cumulative explained Var');
+    plt.xlim(0, 20);
+    plt.show()
+
+
+def kmeans_wcss(n_components, scaled_refl, max_n_clusters=20):
+    '''
+    Returns a list of wcss values for different n_clusters values
+    after performing PCA using n_components.
+    args:
+        n_components   - number of components to be kept in PCA.
+        scaled_refl    - scaled values on which to perform PCA and
+                         clustering.
+        max_n_clusters - max number of clusters to try default 20.
+    '''
+    # use n components for pca
+    pca = PCA(n_components=n_components)
+
+    # fit
+    pca.fit(scaled_refl)
+
+    # get component scores
+    scores_pca = pca.transform(scaled_refl)
+
+    # empty list for witih cluster sum of squares
+    wcss = []
+
+    # now try out some differnt cluster numbers
+    print(f'Out of {max_n_clusters} trials working on:')
+
+    for n in range(1, max_n_clusters + 1):
+
+        print(f'\b\b{n}', end="")
+
+        kmeans =  KMeans(n_clusters=n, init='k-means++', random_state=42)
+        kmeans.fit(scores_pca)
+        wcss.append(kmeans.inertia_)
+
+    print('\ndone!')
+
+    return wcss, pca
+
 
 
 if __name__ == '__main__':
@@ -122,9 +214,9 @@ if __name__ == '__main__':
         globstring = os.path.join(
             args.src,
             year,
-            Fullsite,
+            'Fullsite',
             'D??',
-            f'{year}_{site}_*',
+            f'{year}_{args.site}_*',
             'L3',
             'Spectrometer',
             'Reflectance',
@@ -135,4 +227,36 @@ if __name__ == '__main__':
 
     # ensure directory for pca tiffs exists and make path
     os.makedirs(args.pca, exist_ok=True)
-    
+
+    # begin to make a the path to the hyperspectral data
+    hyper_path = os.path.join(args.data_dir,
+                              args.site,
+                              'hyperspectral',
+                              'DP3.30006.001',
+                              'neon-aop-products')
+
+    # check to make sure there is hyperspectral for the site
+    if not os.path.isdir(hyper_path):
+        a = f'There appears to be no hyperspectral data for {args.site} '
+        b = f'{hyper_path} is not a directory.'
+        msg = a + b
+        raise Exception(msg)
+
+    # get the years (they are directories in hyper_path)
+    years = os.listdir(hyper_path)
+
+    for year in years:
+
+        # get the rest of the path
+        globstring = os.path.join(hyper_path,
+        year,
+        'FullSite',
+        'D08',
+        f'{year}_{args.site}_[0-9]',
+        'L3',
+        'Spectrometer',
+        'Reflectance',
+        f'NEON_D08_{args.site}_DP3_*.h5')
+
+        # find the names of the hyperspectral cubes
+        files = glob.glob(globstring)
--- steps.md
+++ steps.md
@@ -10,8 +10,72 @@
 
 # --------------
 
-1. Downloaded bundled eddy flux data (DP4.00200.001) and the hyperspectral data (DP3.30006.001) for the years 2018-2021 for selected sites (using `get_flux.R`, which is based on `neonUtilities`, and `download.sh` which was generated in `site_download_and_qfqm_check.ipynb`)
-2. For each site, counted the number of half-hour observations in which footprint calculations and NSAE for CO$_{2}$, H$_{2}$O, and temperature all passed the final QAQC tests.  Dates and sites with valid observations are listed in table \ref{make_table_from_df} along with the number of valid observations of each metric. 17,662 observations, across 27 sites fit these criteria. Selecting only for sites with more than 100 observations we end up with 17,643 observations across 24 sites.  For each site with more than 100 valid observations a file was created with the metrics used in footprint calculations and NSAE for CO$_{2}$, H$_{2}$O and T.
-3. 
+1. **Downloaded bundled eddy flux data** - Downloaded bundled eddy flux data (DP4.00200.001) and the hyperspectral data (DP3.30006.001) for the years 2018-2021 for selected sites (using `get_flux.R`, which is based on `neonUtilities`, and `download.sh` which was generated in `site_download_and_qfqm_check.ipynb`)
+```
+NEON
+|__SITE
+   |__footprints
+      |__20180510T233000Z.tiff
+      |__ ...
+      |__20180731T233000Z.tiff
+      |__summary.tiff
+```
 
+2. **Make file of valid flux observations for each site** - For each site, counted the number of half-hour observations in which footprint calculations and NSAE for CO$_{2}$, H$_{2}$O, and temperature all passed the final QAQC tests.  Dates and sites with valid observations are listed in table \ref{make_table_from_df} along with the number of valid observations of each metric. 17,662 observations, across 27 sites fit these criteria. Selecting only for sites with more than 100 observations we end up with 17,643 observations across 24 sites.  For each site with more than 100 valid observations a file was created (`flux_data.csv`) with the metrics used in footprint calculations and NSAE for CO$_{2}$, H$_{2}$O and T. (this step happens in `site_download_and_qfqm_check.ipynb`).
 
+```
+NEON
+|__SITE
+   |__flux_data.csv
+   |
+   |__footprints
+      |__20180510T233000Z.tiff
+      |__ ...
+      |__20180731T233000Z.tiff
+      |__summary.tiff
+```
+
+3. **Built hyperspectral mosaic** - (this happens for each site in `hyperspectral_mosaic_SITE.ipynb`)
+
+```
+NEON
+|__TALL
+   |__flux_data.csv
+   |
+   |__footprints
+   |  |__20180510T233000Z.tiff
+   |  |__ ...
+   |  |__20180731T233000Z.tiff
+   |  |__summary.tiff
+   |
+   |__mosaic
+   |   |__2018
+   |   |  |__TALL_2018_kmeans_mosaic.tiff
+   |   |  |__TALL_2018_pca_mosaic.tiff
+   |   |  |__TALL_2018_mosaic.nc
+   |   |
+   |   |__...
+   |   |  |__ ...
+   |   |
+   |   |__2021
+   |      |  |__TALL_2018_kmeans_mosaic.tiff
+   |      |  |__TALL_2018_pca_mosaic.tiff
+   |      |  |__TALL_2018_mosaic.nc
+   |
+   |__pca
+      |__2018
+      |  |__TALL_460000_3642000_pca.tiff
+      |  |__ ...
+      |  |__TALL_466000_3648000_pca.tiff
+      |
+      |__...
+      |  |__ ...
+      |
+      |__2021
+         |  |__TALL_460000_3642000_pca.tiff
+         |  |__ ...
+         |  |__TALL_466000_3648000_pca.tiff
+```
+4. **Quantize the footprints** - (`Quantize footprint.py`)
+
+5. Revisit giannico method ipynbs
